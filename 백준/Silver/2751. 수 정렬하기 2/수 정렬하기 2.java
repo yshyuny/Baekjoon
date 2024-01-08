06a4df6bb3dfb39543a7665e45cf14a01c7a5c07
yshyuny <152057382+yshyuny@users.noreply.github.com>
2024-01-09 00:10:08 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1548 ms, Memory: 166520 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,3 +1,4 @@
+/* 백준 2751번 수 정렬하기2 */
 import java.io.*;
 import java.util.*;
 
@@ -15,10 +16,10 @@ public class Main {
 			list.add(Integer.parseInt(br.readLine()));
 		}
 		
-		Collections.sort(list);
+		Collections.sort(list); //list에서 오름차순 정렬
 
-		for(int i = 0; i < N; i++) {
-			sb.append(list.get(i)).append("\n");
+		for(int i = 0; i < N; i++) { // for(int 변수명 : list) for문도 알아둘 것~
+			sb.append(list.get(i)).append("\n"); //리스트에서 값 불러올때 list.get()
 		}
 		
 		System.out.println(sb);
