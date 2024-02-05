711689fbb4b4469f08ee657fcb532712e78c302d
yshyuny <152057382+yshyuny@users.noreply.github.com>
2024-02-05 14:45:21 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 160 ms, Memory: 23020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,11 +1,11 @@
 import java.util.*;
 import java.io.*;
- 
+
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main (String[] args) throws IOException {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Queue<Integer> q = new LinkedList<>();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -17,27 +17,24 @@ public class Main {
 		for(int i = 1; i <= N; i++) {
 			q.offer(i);
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append('<');
-		
-		/*
-		 *  마지막 부분의 출력은 > 괄호 전에 공백이 없기 때문에
-		 *  일괄적으로 출력하기 위해 마지막 원소만 남겨질 때까지만
-		 *  반복하고 마지막 원소는 그대로 출력한다.
-		 */
+
+		StringBuilder sb =new StringBuilder();
+		sb.append("<");
+
 		while(q.size() > 1) {
-			
-			for(int i = 0; i < K - 1; i++) {
-				q.offer(q.poll());
-			}
-			
-			sb.append(q.poll()).append(", ");
+
+		for(int i =0; i < K-1; i++) {
+
+			q.offer(q.poll());		
 		}
- 
-		// 마지막 원소 출력한 뒤 > 도 붙여준다.
-		sb.append(q.poll()).append('>');
-		System.out.println(sb);
+
+		sb.append(q.poll()).append(", ");
+
+	}
+
+	sb.append(q.poll()).append(">");
+	System.out.println(sb);
 	}
  
 }
+		
