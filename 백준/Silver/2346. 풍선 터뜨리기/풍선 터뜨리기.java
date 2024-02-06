585ce0469d27681b5e11caddebe3a07e7270ba92
yshyuny <152057382+yshyuny@users.noreply.github.com>
2024-02-06 23:00:25 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 200 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,28 +1,33 @@
 import java.util.*;
+import java.io.*;
 
-class Balloon {
-    int index; // 번호
-    int value; // 풍선에 적힌 값
-    
-    public Balloon(int index, int value) {
-        this.index = index;
-        this.value = value;
-    }
+class Balloon{
+	int index;
+	int value;
+
+public Balloon(int index, int value) {
+	this.index = index;
+	this.value = value;
+	}
 }
 
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Deque<Balloon> dq = new ArrayDeque<>(); // LinkedList는 시간 초과 나기 때문에 ArrayDeque 사용
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i = 1; i <= n; i++) {
-            int value = sc.nextInt();
-            dq.add(new Balloon(i, value));
-        }
-        
-        while(!dq.isEmpty()) {
+	public static void main(String[] args) throws IOException {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int n = Integer.parseInt(br.readLine());
+
+	Deque<Balloon> dq = new ArrayDeque<>(); //LinkedList는 메모리 초과로 ArrayDeque를 써야함
+	StringBuilder sb = new StringBuilder();
+
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	for(int i=1; i <=n; i++)  {
+	
+	 dq.offer(new Balloon(i, Integer.parseInt(st.nextToken())));
+	}
+
+	while(!dq.isEmpty()) {
             // 덱의 맨앞의 풍선 번호 저장
             sb.append(dq.getFirst().index + " ");
             
@@ -36,13 +41,12 @@ public class Main {
             
             // 풍선에 적혀있던 값이 양수인 경우 
             if(num > 0) {
-                for(int i = 0; i < num - 1; i++) { // 앞에서 poll 메소드 사용해서 자동으로 -1 
-                                                    //되었기때문에 num보다 1 작게 이동해야 함
+                for(int i = 0; i < num - 1; i++) { // 앞에서 poll 메소드 사용해서 자동으로 -1 되었기 때문에 num보다 1 작게 이동해야 함
                     dq.addLast(dq.pollFirst());
                 }
             } else {
-                for(int i = 0; i < Math.abs(num); i++) { // 음수일땐 해당값까지 앞으로 보내야해서
-                    dq.addFirst(dq.pollLast());        // -1 안함
+                for(int i = 0; i < Math.abs(num); i++) {
+                    dq.addFirst(dq.pollLast());
                 }
             }
         }
